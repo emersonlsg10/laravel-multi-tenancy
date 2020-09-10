0f1258b204096be75a5970cd39536062fea5bbc7
emersonlsg10 <you@example.com>
2020-09-09 21:16:46 -0300

    tvtvtvtvtv

--- resources/views/tenant/index.blade.php
+++ resources/views/tenant/index.blade.php
@@ -15,7 +15,7 @@
 <div class="col-xl-12">
     <div class="row">
         <div class="col-md-3">
-            <div class="card">
+            <div class="card x">
                 <div class="card-body">
                     <div class="media">
                         <div class="media-body overflow-hidden">
